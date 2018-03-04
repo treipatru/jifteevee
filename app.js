d8ba2944d58ff79f4c12f982eefdfb8e53611359
Andrei34 <andrei@planet34.org>
2018-03-04 16:31:28 +0100

    Cleanup isle 5

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-require('dotenv').config();
-require('./scripts/bot');
-
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+require('./scripts/bot');
 
 let index = require('./routes/index');
-let users = require('./routes/users');
-
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

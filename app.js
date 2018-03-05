b8df9c89b58f2445b20ba48e7a56837cff3401b2
Andrei34 <andrei@planet34.org>
2018-03-06 00:15:11 +0100

    Init application controller

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 
 require('dotenv').config()
-require('./scripts/bot')
+require('./scripts/socketServer')
+require('./scripts/jifmachine')
 
 // ROUTES
 // --------------------------
 let index = require('./routes/index')
+
+// APP
+// --------------------------
 let app = express()
 
-// VIEW ENGINE
-// --------------------------
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
@@ -25,20 +27,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   let err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500)
   res.render('error')
 })

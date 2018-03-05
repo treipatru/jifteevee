83e79e7f6f3df94595f4c27d0deec6df4287ecb7
Andrei34 <andrei@planet34.org>
2018-03-06 00:13:01 +0100

    Removed obsolete vars

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const router = express.Router()
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'JIFTEEVE',
-    message: process.env.KEY_GIPHY,
-    gifUrl: 'https://media.giphy.com/media/1k2YhdutgkQzJWnsyp/giphy.gif'
+    title: 'JIFTEEVE'
   })
 })
 

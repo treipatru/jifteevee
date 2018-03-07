4bc2390d2823063c1eccd0345f9fa9bd7fe1a007
Andrei34 <andrei@planet34.org>
2018-03-07 23:45:46 +0100

    First iteration of functional telegram-giphy exchange with client update

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,11 +1,32 @@
+const bus = require('./eventBus')
 const Telegraf = require('telegraf')
+const axios = require('axios')
 
+// BOT
+// --------------------------
 const bot = new Telegraf(process.env.KEY_TELEGRAM)
-bot.start((ctx) => {
-  console.log('started:', ctx.from.id)
-  return ctx.reply('Welcome!')
-})
-bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.on('text', (ctx) => getGiphy(ctx.message.text))
 bot.startPolling()
+
+// GIPHY
+// --------------------------
+let getGiphy = function (query) {
+  axios.get('http://api.giphy.com/v1/gifs/search', {
+    params: {
+      q: query,
+      api_key: process.env.KEY_GIPHY,
+      limit: 1
+    }
+  })
+    .then(function (r) {
+      updateLiveUrl(r.data.data[0].images.original.url)
+    })
+    .catch(function (e) {
+      console.log(e)
+    });
+}
+
+let updateLiveUrl = function (data) {
+  bus.emit('gifNext', data)
+}

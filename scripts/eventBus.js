d16ccf32228f8f6ccbe1a94c481f84451bf9c00a
Andrei34 <andrei@planet34.org>
2018-03-07 23:44:25 +0100

    Added liveUrl getter/setter, gif url broadcast

--- scripts/eventBus.js
+++ scripts/eventBus.js
@@ -1,4 +1,15 @@
 const events = require('events')
+const wss = require('./socketServer')
 let emitter = new events.EventEmitter()
+
+// Gif events
+let liveUrl = ''
+
+let gifNext = function (data) {
+  liveUrl = data
+  wss.broadcast(data)
+  console.log('changed to ' + data)
+}
+emitter.on('gifNext', gifNext);
 
 module.exports = emitter
